f5955fcfef6c76098e23054d33f5c0b6ed32a34c
Tham53 <59705897+Tham53@users.noreply.github.com>
2020-05-30 20:36:39 -0500

    Update main.js

--- main.js
+++ main.js
@@ -15,9 +15,8 @@ let moreCars = ['Audi', 'Toyota', 'Infinity', 'Honda']
 
 
 //totalCars array (combined)
-let totalCars = cars.concat(moreCars);
-console.log("totalCars", totalCars)
-
+let totalCars1 = cars.concat(moreCars);
+console.log("totalCars1", totalCars1)
 
 // 3// .indexOf() and .lastIndexOf()
 // Use the indexOf method to console.log the index of Honda.
@@ -26,29 +25,23 @@ console.log("totalCars", totalCars)
 console.log(moreCars.indexOf('Honda'))
 console.log(moreCars.lastIndexOf('Ford'))
 
-
 // 4// .join()
 // Use the join method to covert the array totalCars into a string called stringOfCars.
 // Run the program.
-console.log(totalCars.join());
-
-
-
+console.log(totalCars1.join());
+let stringOfCars = (totalCars1.join());
 
 // 5// .split()
 // Use the split method to convert stringOfCars back intro an array called totalCars.
 // Run the program.
-let totalCars1 = stringOfCars.split(" ");
-
-
+let totalCars = stringOfCars.split(",");
+console.log(totalCars)
 
 // 6// .reverse()
 // Use the reverse method to create an array carsInReverse which is the array totalCars in reverse.
 // Run the program.
-console.log(totalCars1.reverse());
-var carsInReverse = totalCars1
-
-
+var carsInReverse = totalCars.reverse();
+console.log(carsInReverse)
 
 // 7// .sort()
 // Use the sort method to put the array carsInReverse into alphabetical order.
@@ -56,47 +49,48 @@ var carsInReverse = totalCars1
 // Use the following code to confirm or reject your prediction:
 //   alert(carsInReverse.indexOf('yourPrediction'));
 // Run the program.
-totalCars.sort()
-
-
+carsInReverse.sort()
+console.log(carsInReverse)
 
 // 8// .slice()
 // Use the slice method to remove Ford and Honda from the carsInReverse array and move them into a new array called removedCars.
-let removedCars = totalCars.slice(2,4)
-
+let removedCars = carsInReverse.slice(2,4)
+console.log(carsInReverse)
 
 
 // 9// .splice()
 // Use the splice method to remove the 2nd and 3rd items in the array carsInReverse and add Ford and Honda in their place.
-carsInReverse
-
-
+// totalCars.splice(2,3);
+carsInReverse.splice(2,2)
+console.log(carsInReverse)
 
 // 10// .push()
 // Use the push method to add the types of cars that you removed using the splice method to the carsInReverse array.
-carsInReverse
+carsInReverse.push("Ford", "Honda")
+console.log(carsInReverse)
 
 
 
 // 11// .pop()
 // Use the pop method to remove and console.log the last item in the array carsInReverse.
-carsInReverse
+carsInReverse.pop();
 
 
 
 // 12// .shift()
 // Use the shift method to remove and console.log the first item in the array carsInReverse.
-carsInReverse
+carsInReverse.shift();
 
 
 
 // 13// .unshift()
 // Use the unshift method to add a new type of car to the array carsInReverse.
-carsInReverse
+carsInReverse.unshift();
 
 
 
 // 14// .forEach()
 // Create an array called numbers with the following items: 23, 45, 0, 2 Write code that will add 2 to each item in the array numbers.
 // .forEach() requires a function to be passed into it as its first argument. Build a function that will add 2 and then use .forEach() to pass each number into your freshly built function. const numbers = [23, 45, 0 , 2, 8, 44, 100, 1, 3, 91, 34]
-
+let numbers = ['23', '45', '0', '2'].forEach(function(item))
+console.log(numbers)
